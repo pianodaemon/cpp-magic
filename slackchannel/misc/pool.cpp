e20a0883b6579384c3c6035bf333b187e37bd82a
Bob Ross <pianodaemon@gmail.com>
2018-10-02 23:44:50 -0500

    slackchannel : first lines of req_next
    implementing place_smart best case scenario.
    
    Signed-off-by: Bob Ross <pianodaemon@gmail.com>

--- slackchannel/misc/pool.cpp
+++ slackchannel/misc/pool.cpp
@@ -50,8 +50,43 @@ Misc::Pool<T>::place_smart( T* t )
 {
     auto req_next = [&]()->int
     {
+        uint8_t i = this->m_registers[ Regs::NEXT_REG ];
+
+        if ( ( this->m_slots[ i ] != nullptr ) &&
+                ( i == this->m_registers[ Regs::LAST_REG ] ) )
+        {
+            // From the first shelf we shall start
+            // the quest of an available one if
+            // next one was ocuppied and the last one.
+            i = this->m_registers[ Regs::START_REG ];
+        }
+
+        if ( this->m_slots[ i ] == nullptr )
+        {
+            //  When the shelf is available we shall return it
+            //  before we shall set next_num variable up for
+            //  later calls to current function.
+            if ( index == this->m_registers[ Regs::LAST_REG ] )
+            {
+                this->m_registers[ Regs::NEXT_REG ] = this->m_registers[ Regs::START_REG ];
+            }
+            else
+            {
+                uint8_t next_one = i + this->m_registers[ Regs::INCREMENT_REG ];
+                this->m_registers[ Regs::NEXT_REG ] = next_one;
+            }
+
+            return i;
+        }
+
+        // If you've reached this code block my brother, so...
+        //   you might be in trouble soon. By the way you seem
+        //   a lucky folk and perhaps you would find a free
+        //   shelf by performing sequential search with awful
+        //   linear time. Otherwise the matter is fucked :(
+
         /* still needed implementation*/
-        return 0;
+
     };
 
     this->m_mutex.lock();
